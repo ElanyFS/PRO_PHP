c92f0f60cd1cad1ffc20acc4d49cd45d4a77c575
Elany Ferreira de Souza <elany.ferreira@seati.ma.gov.br>
2023-01-19 14:15:02 -0300

    conexao

--- app/database/connect.php
+++ app/database/connect.php
@@ -1,8 +1,15 @@
 <?php
 
+// function connect()
+// {
+//     return new PDO('mysql:host=127.0.0.1;dbname=phppro', 'root','root', [
+//         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ
+//     ]);
+// }
+
 function connect()
 {
-    return new PDO('mysql:host=127.0.0.1;dbname=phppro', 'root','root', [
+    return new PDO('mysql:host=127.0.0.1;dbname=phppro', 'root','admin', [
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ
     ]);
 }
