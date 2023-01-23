e13b2fa7f2d17f76f3b7af1c2d07576b9d8499fa
Elany Ferreira de Souza <elany.ferreira@seati.ma.gov.br>
2023-01-23 16:17:34 -0300

    requisicao ajax

--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -7,7 +7,7 @@ class Users
     public function index()
     {
         // echo json_encode($_SERVER);
-        $users = all('usuarios', 'idusuarios,nome_user');
+        $users = all('usuarios', 'idusuario,nome_user');
 
         echo json_encode($users);
     }
