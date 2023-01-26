6172eab6f2314605c18886d2cda402275beb73f9
Elany Souza <ellanysouza08@gmail.com>
2023-01-25 23:42:05 -0300

    update

--- app/database/fetch.php
+++ app/database/fetch.php
@@ -68,29 +68,29 @@ function execute()
     return $prepare->fetchAll();
 }
 
-// function all($table, $fields = '*')
-// {
-//     try {
-//         $connect = connect();
+function all($table, $fields = '*')
+{
+    try {
+        $connect = connect();
 
-//         $query = $connect->query("Select {$fields} from {$table}");
-//         return $query->fetchAll();
-//     } catch (PDOException $e) {
-//         var_dump($e->getMessage());
-//     }
-// }
+        $query = $connect->query("Select {$fields} from {$table}");
+        return $query->fetchAll();
+    } catch (PDOException $e) {
+        var_dump($e->getMessage());
+    }
+}
 
-// function findBy($table, $fields = '*', $field, $value)
-// {
-//     try {
-//         $connect = connect();
+function findBy($table, $fields = '*', $field, $value)
+{
+    try {
+        $connect = connect();
 
-//         $prepare = $connect->prepare("select {$fields} from {$table} where {$field} = :{$field}");
-//         $prepare->execute([
-//             $field => $value
-//         ]);
-//         return $prepare->fetchObject();
-//     } catch (PDOException $e) {
-//         var_dump($e->getMessage());
-//     }
-// }
+        $prepare = $connect->prepare("select {$fields} from {$table} where {$field} = :{$field}");
+        $prepare->execute([
+            $field => $value
+        ]);
+        return $prepare->fetchObject();
+    } catch (PDOException $e) {
+        var_dump($e->getMessage());
+    }
+}
